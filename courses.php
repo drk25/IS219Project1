e6866b3879cb90e32ada39a892c07a6f688eaf6a
drk25 <drk25@njit.edu>
2017-02-19 20:13:02 -0500

    Key courses and Projects
    Key courses and Projects

---
+++ courses.php
@@ -0,0 +1,45 @@
+	<?php include("includes/header.php") ?>
+<title>Key Courses and Projects </title>
+	<body>
+		<div class="panel panel-default font_style container container-fluid">
+		<div class="panel-heading">Core Courses</div>
+			<div class="panel-body">
+				<ul>
+					<li>Introduction to Data Structures and Algorithms</li>
+					<li>Programming language Concepts</li>
+					<li>Intensive Programming in Linux</li>
+					<li>Operating Systems</li>
+					<li>Introduction to Networking</li>
+					<li>Database Management Systems</li>
+				</ul>				
+			</div>
+		
+		<div class="panel-heading">Minor Courses</div>
+			<div class="panel-body">
+				<ul>
+					<li>Introduction to Website Development</li>
+					<li>Building web Applications</li>
+					<li>Advanced Website Development</li>
+					<li>Requirements Analysis and System Design </li>
+				</ul>	
+			</div>
+			
+			<div class="panel-heading">Projects</div>
+			<div class="panel-body">
+			<ul>
+				<li>Built a web application using MVC framework in PHP.</li>
+				<li>Created an e-commerce website for sport equipment using Bootstrap</li>
+				<li>Made a file handling program, which takes multiple arguments and output the result based on the argument.</li>
+				<li>Built Lexical analyzer, which parses custom made programming language.</li>
+				<li>Developed a university database system, which store and handle all the records for students and faculties.</li>
+				<li>Created a Deterministic Finite Automata (DFA) to check URL.</li>
+			</ul>	
+			</div>
+			<ul class="pager font_style">
+				<li><a href="index.php">Previous</a></li>
+				<li><a href="contact.php">Next</a></li>
+			</ul>
+
+<?php include("includes/footer.php") ?>
+	</body>
+</html>
