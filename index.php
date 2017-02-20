c65ad3e365c725140cf17df66b5a6ae8af7a72f7
drk25 <drk25@njit.edu>
2017-02-19 20:12:26 -0500

    index.php
    HomePage

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php include("includes/header.php") ?>
+		
+		<div class="container">
+				<h2>Hello, I'm Dhawal Khatiwala</h2>
+		</div>
+		
+<img src="http://placehold.it/500x250" class="img-thumbnail" alt="profile_dp" id="img">    
+
+
+<div class = "container">
+<div class = "row">
+<p><strong>My Background</p></strong>
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+</div>
+
+				<div class="panel panel-default container container-fluid font_style">
+				<h3>About Me</h3>
+						<ul>
+							<li>Date of Birth : Nov 26, 1992</li>
+							<li>E-Mail : dhawal.khatiwala@gmail.com</li>
+							<li>Contact Number : 732-749-2112</li>
+							<li>Linked IN <a href="#"><i class="fa fa-linkedin"></i> :Dhawal Khatiwala</a>
+						</ul>
+						<h3>Residence Address</h3>
+						<ul>
+							<li><p> 25 Branch Brook Pl, 
+									Apartment 3F, 
+									Newark, NJ-07104</p></li>
+						</ul>
+						<h3>Computer Skills</h3>
+						<div class="panel panel-default">
+							<div class="panel-body">
+								<p>	Front end Technologies: HTML5, CSS3, Bootstrap, MVC framework.<br>
+									Coding Languages:	C/C++, Python/PHP, Java<br>
+									Familiar: JavaScript<br>
+									Database: MySQL<br>
+									IDE: Eclipse, NetBeans, Visual Studio, Sublime Text</p>
+						
+							</div>
+						</div>
+						<br>
+						<div class="container container-fluid font_style">
+							<h3>Educational Background</h3>
+						</div>
+						<div class="panel panel-default font_style">
+							<div class="panel-heading">Pursuing Bachlors's In <strong>Computer Sciences</strong> and minor in <strong>Web and Information Systems.</strong></div>
+								<div class="panel-body">
+									<ul class="list-group">
+										<li class="list-group-item">School : New Jersey Institute of Technology</li>
+				 						<li class="list-group-item">G.P.A : 2.8</li>
+				 						<li class="list-group-item">Graduation : May 2018</li>
+				 					</ul>
+								</div>
+							</div>
+<!-- 							<div class="panel-heading">Guitar School</div> -->
+								<div class="panel-body">
+									<div class="row">
+										<div class="col-sm-4">
+<!-- 											<p>Learnt Music Theory and Playing guitar from Delhi School Music.</p> -->
+										</div>
+										<div class="col-sm-4">
+<!-- 											<p>Obtained Grade 1 Music Theory and Practical Certifications from Trinity College,London.</p> -->
+										</div>
+										<div class="col-sm-4">
+<!-- 											<p>Obtained Grade 2 Music Theory Certification from Associate Board, Royal School of Music,London.</p> -->
+										</div>
+									</div>
+								</div>
+							<div class="panel-heading">School</div>
+							<div class="panle-body container container-fluid">
+								<p>Completed High School in Gujarat, India.</p>
+							</div>
+						</div>
+						<ul class="pager font_style">
+							<li><a href="courses.php">Next</a></li>
+						</ul>
+					</div>
+
+
+
+<?php include("includes/footer.php") ?>				
+			
+		<script type="text/javascript" src="js/bootstrap.js"></script> 
+		<script type="text/javascript" src="js/bootstrap.min.js"></script> 
+		<script type="text/javascript" src="js/jquery.js"></script> 
+	</body>
+</html>
